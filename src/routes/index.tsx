2acb75fea16a569f08038fb40a1ce09ca82e8719
Nate <natewbrooks@gmail.com>
2026-02-07 23:49:21 -0500

    Tag validation and enter key

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,8 @@ import logo from "../../public/brand/textLogo.svg";
 import { Icon } from "@iconify/react";
 import upArrow from "../../public/upArrow.svg";
 import { validateTag, normalizeTagInput } from "../utils/tag";
-import { cn } from "../lib/utils";
+import { useRouter } from "@tanstack/react-router";
+
 
 export const Route = createFileRoute("/")({
   component: HomeComponent,
@@ -12,15 +13,8 @@ export const Route = createFileRoute("/")({
 
 function HomeComponent() {
   const [tag, setTag] = useState<string>("");
-  const [validated, setValidated] = useState<string | null>(null);
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const v = validateTag(tag);
-    setValidated(v);
-    if (!v) return;
-  };
-
+  const router = useRouter();
   const isValid = !!validateTag(tag);
 
   return (
@@ -34,35 +28,38 @@ function HomeComponent() {
       <div className="flex justify-center items-center">
         <img src={logo} width={150} />
       </div>
-      <div>
-        <h3 className="text-white/20 text-2xl tracking-wide">
+      <div className="flex flex-col -space-y-1">
+        <h3 className="text-white/20 text-3xl tracking-wide">
           Enter a <span className="text-green">slippi</span> player tag:
         </h3>
-        <form className="flex items-center justify-center" onSubmit={onSubmit}>
-          <input
-            className="bg-darkest w-full text-8xl text-white placeholder:text-white/20 h-fit leading-0 px-4 flex-1 outline-none"
-            placeholder="TAG#000"
-            value={tag}
-            onChange={(e) => setTag(normalizeTagInput(e.target.value))}
-          />
-          <Link
-            to="/user/$tag"
-            params={{ tag: tag.replace("#", "-") }}
-            onClick={(e) => {
-              if (!isValid) {
-                e.preventDefault();
-                e.stopPropagation();
-              }
-            }}
-            className={cn(
-              "w-fit p-4 h-full bg-red transition-opacity",
-              !isValid && "opacity-50 pointer-events-none"
-            )}
-          >
-            <img src={upArrow} width={42} className="rotate-90 text-white" />
-          </Link>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (!isValid) return;
 
-        </form>
+          router.navigate({
+            to: "/user/$tag",
+            params: { tag: tag.replace("#", "-") },
+          });
+        }}
+        className="flex items-center justify-center"
+      >
+        <input
+          value={tag}
+          onChange={(e) => setTag(normalizeTagInput(e.target.value))}
+          className="bg-darkest w-full text-8xl text-white/20 px-4 flex-1"
+          placeholder="TAG#000"
+        />
+
+        <button
+          type="submit"
+          disabled={!isValid}
+          className="w-fit p-4 h-full bg-red disabled:opacity-50"
+        >
+          <img src={upArrow} width={42} className="rotate-90 text-white" />
+        </button>
+      </form>
+
         {tag.length > 0 && !validateTag(tag) ? (
           <div className="text-red mt-2">Invalid tag</div>
         ) : null}
